88a5ad2cbe3ded9bcdb78870a8036fb983798eaf
chenguang du <duchengaung@licaigc.com>
2019-09-11 19:36:23 +0800

    temp changes

--- sql/future_backtest_instance.sql
+++ sql/future_backtest_instance.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `future_instance` (
+CREATE TABLE `future_strategy_instance` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `symbol` varchar(30) NOT NULL,
   `exchange` varchar(20) NOT NULL,

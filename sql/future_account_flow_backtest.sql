018a9da03e2c928ce0ecb1a05a0314f95dfece13
chenguang du <duchengaung@licaigc.com>
2019-11-28 19:37:31 +0800

    should change the table struct

--- sql/future_account_flow_backtest.sql
+++ sql/future_account_flow_backtest.sql
@@ -2,7 +2,6 @@ CREATE TABLE `future_account_flow_backtest` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `symbol` varchar(30) NOT NULL,
   `exchange` varchar(20) NOT NULL,
-  `contract_type` varchar(20) NOT NULL DEFAULT '' COMMENT 'contracty type: this_week/next_week/quarter',
   `backtest_id` varchar(32) NOT NULL COMMENT 'backtest unique id',
   `subject` varchar(30) NOT NULL COMMENT 'injection/dividend/freeze/unfreeze/income/transaction_fee/adjustment/transfer',
   `amount` bigint(20) NOT NULL COMMENT 'the real amount * 100000000',

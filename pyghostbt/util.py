28cce0f30fc64f9393491de1a4a0ebd73dddcf21
chenguang du <duchengaung@licaigc.com>
2019-12-18 20:07:39 +0800

    add some util func

--- pyghostbt/util.py
+++ pyghostbt/util.py
@@ -1,5 +1,8 @@
 import hashlib
 import random
+import math
+
+from typing import List
 from datetime import datetime
 
 from pyanalysis.moment import moment
@@ -37,8 +40,72 @@ def standard_number(input_num: float) -> int:
     return int(input_num * 100000000 - 0.5)
 
 
-def real_number(std_num: int) -> float:
+def real_number(std_num: float) -> float:
     return std_num / 100000000
+
+
+def get_o_or_l_price(
+        anchored_price: int = None,
+        diff: int = None,
+        open_times: int = None,
+) -> List[int]:
+    return [anchored_price + i * diff for i in range(open_times)]
+
+
+def get_open_amount(
+        total_asset: float = None,
+        position: float = None,
+        unit_amount: int = None,
+        slippage: float = None,
+        open_price: int = None,
+        liquidate_price: int = None,
+        open_times: int = 3,
+        fee_rate: float = -0.0005,  # The fee_rate must be negative.
+        scale: float = 1.0,
+) -> List[int]:
+    """Get the open amount array.
+
+    The open amount is a complicated calculating process. This func calculate it for worst situation. Why complicated?
+    1. total_asset包含了fee + 保证金。其中fee是损失，所以total_asset并不能当做净值计算open_amount。
+    2. 开仓平仓会有滑点，滑点会带来fee的变动，以及open_amount的变动。
+
+    故此方法假设，此次交易以最差的情况止损。并在开仓，平仓都有滑点的情况下完成交易。
+
+    Args:
+        total_asset: The total asset of the pair
+        position: The position want to open.
+        unit_amount: The contract unit amount.
+        slippage: The slippage must be negative.
+        open_price: The avg_price of open orders.
+        liquidate_price: The avg_price of liquidate orders.
+        open_times: The times to divide.
+        fee_rate: The fee rate
+        scale:
+
+    Returns:
+        amount array of each open times.
+    """
+    real_open_price = real_number(open_price)
+    real_liquidate_price = real_number(liquidate_price)
+    worst_real_price = min(real_open_price, real_liquidate_price)
+
+    # 粗略的开仓张数
+    open_amount = total_asset * position * worst_real_price * (1.0 + slippage) / unit_amount
+
+    # 计算净资产
+    max_open_fee = total_asset * position * fee_rate
+    max_liquidate_fee = open_amount * unit_amount * fee_rate / (real_liquidate_price * (1.0 + slippage))
+    net_asset = total_asset + max_open_fee + max_liquidate_fee
+    open_amount = net_asset * position * worst_real_price * (1.0 + slippage) / unit_amount
+
+    amounts: List[int] = []
+    sum_scale = scale * open_times
+    if scale != 1.0:
+        sum_scale = (1 - math.pow(scale, open_times)) / (1 - scale)
+    for i in range(open_times):
+        amount = int(open_amount * math.pow(scale, i) / sum_scale)
+        amounts.append(amount)
+    return amounts
 
 
 def get_contract_type(timestamp: int, due_timestamp: int) -> str:

b887e7f3568b51d161dad09a9a58edaf264c8439
strengthening <ducg@foxmail.com>
2020-04-24 19:25:23 +0800

    some changes of factor

--- pyghostbt/tool/factor.py
+++ pyghostbt/tool/factor.py
@@ -17,7 +17,7 @@ factor_input = {
         },
         "contract_type": {
             "type": "string",
-            "enum": [CONTRACT_TYPE_THIS_WEEK, CONTRACT_TYPE_NEXT_WEEK, CONTRACT_TYPE_QUARTER],
+            "enum": [CONTRACT_TYPE_THIS_WEEK, CONTRACT_TYPE_NEXT_WEEK, CONTRACT_TYPE_QUARTER, CONTRACT_TYPE_NONE],
         },
         "db_name": {
             "type": "string",

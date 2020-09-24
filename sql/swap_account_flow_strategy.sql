8fa7a28b379ff87150a3459d9bb30b408e3da84d
chenguang du <duchengaung@licaigc.com>
2020-09-24 09:10:49 +0800

    fix the the sql

--- sql/swap_account_flow_strategy.sql
+++ sql/swap_account_flow_strategy.sql
@@ -2,7 +2,7 @@ CREATE TABLE `swap_account_flow_strategy` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `symbol` varchar(30) NOT NULL,
   `exchange` varchar(20) NOT NULL,
-  `settle_mode` tinyint(4) NOT NULL DEFAULT '1' COMMENT '1: basis currency, 2: counter currency',
+  `settle_mode` tinyint(4) NOT NULL DEFAULT '0' COMMENT '1: basis currency, 2: counter currency',
   `settle_currency` varchar(20) NOT NULL DEFAULT '' COMMENT 'the settle currency',
   `subject` varchar(30) NOT NULL COMMENT 'injection/dividend/freeze/unfreeze/income/transaction_fee/adjustment/transfer',
   `amount` bigint(20) NOT NULL COMMENT 'the real amount * 100000000',
@@ -12,8 +12,8 @@ CREATE TABLE `swap_account_flow_strategy` (
   `create_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `update_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `symbol` (`symbol`, `exchange`, `settle_mode`, `timestamp`, `subject`),
-  KEY `exchange` (`exchange`, `settle_mode`, `settle_currency`, `timestamp`, `subject`),
+  UNIQUE KEY `symbol` (`symbol`,`exchange`,`settle_mode`,`settle_currency`,`subject`,`timestamp`),
+  KEY `exchange` (`exchange`,`settle_mode`,`settle_currency`,`timestamp`,`subject`),
   KEY `timestamp` (`timestamp`),
   KEY `datetime` (`datetime`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;

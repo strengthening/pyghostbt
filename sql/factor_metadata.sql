b80714457b9b8764fa816f60241f273b8d6b6ef6
chenguang du <duchengaung@licaigc.com>
2021-03-08 17:52:25 +0800

    update the factor table

--- sql/factor_metadata.sql
+++ sql/factor_metadata.sql
@@ -2,10 +2,11 @@ CREATE TABLE `factor_metadata` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `symbol` varchar(20) NOT NULL COMMENT 'The factor symbol',
   `trade_type` varchar(20) NOT NULL COMMENT 'The factor trade type',
+  `interval` varchar(10) NOT NULL DEFAULT '1day' COMMENT 'The factor interval',
   `factor_name` varchar(30) NOT NULL COMMENT 'The factor name',
   `factor_desc` text COMMENT 'The factor desc, include factor raw data, factor calculation formula',
   `create_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `update_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `symbol` (`symbol`, `trade_type`, `factor_name`)
+  UNIQUE KEY `symbol` (`symbol`,`trade_type`,`interval`,`factor_name`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
